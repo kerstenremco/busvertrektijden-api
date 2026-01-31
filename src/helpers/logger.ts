c5f792e0eff72c6dadbec48129adbbeda0412b7a
Remco Kersten <kersten.remco@outlook.com>
2026-01-31 22:51:37 +0100

    Changed logging to cli loki

--- src/helpers/logger.ts
+++ src/helpers/logger.ts
@@ -5,9 +5,9 @@ const logger = winston.createLogger({
   format: winston.format.combine(
     winston.format.errors({ stack: true }),
     winston.format.timestamp(),
-    process.env.LOG_FILE ? winston.format.json() : winston.format.prettyPrint()
+    process.env.LOG_PROD ? winston.format.json() : winston.format.prettyPrint(),
   ),
-  transports: process.env.LOG_FILE ? [new winston.transports.File({ filename: process.env.LOG_FILE })] : [new winston.transports.Console()],
+  transports: [new winston.transports.Console()],
 });
 
 export default logger;
